31333def25861a402cc9b6d9b0ef73f4f5bcfc77
Subeen Park <psb021414@gmail.com>
2021-01-16 15:08:15 +0900

    made first template

--- server/diary/views.py
+++ server/diary/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 # Create your views here.
 
 
 def article_list(request):
-    return render(request, 'diary/list.html', context={})
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'diary/list.html', context=context)
