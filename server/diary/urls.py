4658dda973fc4e9948063a5cbaa5abdd457754fb
Subeen Park <psb021414@gmail.com>
2021-01-16 15:25:03 +0900

    article list/article read

--- server/diary/urls.py
+++ server/diary/urls.py
@@ -5,4 +5,5 @@ app_name = 'diary'
 
 urlpatterns = [
     path('', article_list, name="article_list"),
+    path('article/<int:pk>/', article_read, name='article_read'),
 ]
--- server/diary/views.py
+++ server/diary/views.py
@@ -11,3 +11,11 @@ def article_list(request):
         'articles': articles,
     }
     return render(request, 'diary/list.html', context=context)
+
+
+def article_read(request, pk):
+    article = Article.objects.get(id=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'diary/read.html', context=context)
