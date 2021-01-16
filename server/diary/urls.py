d99600f19efd5c61570d77f1ab6b5b73f6c4bb7b
Subeen Park <psb021414@gmail.com>
2021-01-16 17:03:22 +0900

    add create/delete view

--- server/diary/urls.py
+++ server/diary/urls.py
@@ -6,5 +6,7 @@ app_name = 'diary'
 urlpatterns = [
     path('', article_list, name="article_list"),
     path('article/<int:pk>/', article_read, name='article_read'),
+    path('article/<int:pk>/update/', article_update, name='article_update'),
+    path('article/<int:pk>/delete/', article_delete, name='article_delete'),
     path('article/create/', article_create, name='article_create'),
 ]
--- server/diary/views.py
+++ server/diary/views.py
@@ -7,7 +7,7 @@ from .models import Article
 
 
 def article_list(request):
-    articles = Article.objects.all()
+    articles = Article.objects.all().order_by('-id')
     context = {
         'articles': articles,
     }
@@ -35,3 +35,31 @@ def article_create(request):
 
     url = reverse('diary:article_read', kwargs={'pk': pk})
     return redirect(url)
+
+
+def article_update(request, pk):
+    if request.method == 'GET':
+        article = Article.objects.get(id=pk)
+        context = {
+            'article': article,
+        }
+        return render(request, 'diary/update.html', context=context)
+
+    # post
+    title = request.POST['title']
+    content = request.POST['content']
+
+    article = Article.objects.filter(id=pk).update(
+        title=title, content=content)  # get X filter O for update
+
+    url = reverse('diary:article_read', kwargs={'pk': pk})
+    return redirect(url)
+
+
+def article_delete(request, pk):
+    if request.method == 'GET':
+        return redirect('/')
+
+    article = Article.objects.get(id=pk)
+    article.delete()
+    return redirect('/')
